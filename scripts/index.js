7e4d375e276b9d98059172aac8b9f4ec2ca09296
Beatriz Rodrigues <beatrizrodrrigues@gmail.com>
2023-10-11 19:03:05 -0300

    estilizando inputs e botão

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function blurInput(input) {
+  const idLabel = 'label-' + input.name
+
+  const isValue = input.value != ''
+
+  const label = document.getElementById(idLabel)
+  label.classList = isValue ? 'active' : ''
+  console.log(input);
+}
